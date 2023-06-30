1442ea0a9985d0b5084b8be5e63510b12e638bb0
SofiaPinilla <contact@sofiapinilla.com>
2023-06-30 13:16:52 +0200

    first commit

---
+++ controllers/PostController.js
@@ -0,0 +1,62 @@
+const { Post, User,Sequelize } = require("../models/index.js");
+const {Op}= Sequelize
+
+const PostController = {
+  create(req, res) {
+    Post.create(req.body)
+      .then((post) =>
+        res.status(201).send({ message: "Post successfully creates", post })
+      )
+      .catch((err) => console.error(err));
+  },
+  getAll(req, res) {
+    Post.findAll({
+      include: [User],
+    })
+      .then((posts) => res.send(posts))
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send(err);
+      });
+  },
+  getById(req, res) {
+    Post.findByPk(req.params.id, {
+      include: [User],
+    })
+      .then((post) => res.send(post))
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send(err);
+      });
+  },
+  getOneByName(req, res) {
+    Post.findOne({
+      where: {
+        title: {
+          [Op.like]: `%${req.params.title}%`,
+        },
+      },
+      include: [User],
+    }).then((post) => res.send(post));
+  },
+  async delete(req, res) {
+    try {
+        await Post.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+        res.send(
+            'La publicación ha sido eliminada con éxito'
+        ) 
+    } catch (error) {
+        console.error(error);
+        res.status(500).send(error)
+    }
+    
+},
+
+
+};
+
+module.exports = PostController;
---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const app = express()
+const PORT = 3000
+
+//middlware
+app.use(express.json())
+
+//rutas prefijo
+app.use("/users",require("./routes/users"))
+app.use("/posts",require("./routes/posts"))
+
+
+app.listen(PORT,()=>console.log(`Servidor levantado en el puerto ${PORT}`))
